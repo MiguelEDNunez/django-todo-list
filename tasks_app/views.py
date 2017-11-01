aac6857b70ce9f7be467b7183794cef2eb97fb69
MiguelEDNunez <miguel.ed.nunez@gmail.com>
2017-11-01 16:00:13 +0000

    sign up fix

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -51,7 +51,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_date.get('username')
+            username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
